bd9d9c15785e5fa08bc2edd61e7b964a42f3aad8
Jorge <jorgepri00@gmail.com>
2020-12-11 12:18:00 +0100

    CommitSplash

--- app/src/main/java/com/jorgeprieto/diciembre2020jorgeprieto/ui/home/HomeFragment.kt
+++ app/src/main/java/com/jorgeprieto/diciembre2020jorgeprieto/ui/home/HomeFragment.kt
@@ -4,15 +4,20 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.LinearLayout
 import android.widget.TextView
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProviders
+import androidx.recyclerview.widget.RecyclerView
 import com.jorgeprieto.diciembre2020jorgeprieto.R
 
 class HomeFragment : Fragment() {
 
     private lateinit var homeViewModel: HomeViewModel
+    private lateinit var recyclerView: RecyclerView
+    private lateinit var viewAdapter: RecyclerView.Adapter<*>
+    private lateinit var viewManager: RecyclerView.LayoutManager
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -22,10 +27,17 @@ class HomeFragment : Fragment() {
         homeViewModel =
             ViewModelProviders.of(this).get(HomeViewModel::class.java)
         val root = inflater.inflate(R.layout.fragment_home, container, false)
-        val textView: TextView = root.findViewById(R.id.text_home)
-        homeViewModel.text.observe(viewLifecycleOwner, Observer {
-            textView.text = it
+
+
+
+            homeViewModel.text.observe(viewLifecycleOwner, Observer {
+
+
         })
         return root
     }
+
+
+
 }
+
